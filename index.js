36b69852cc77970b56444fe8cd574fcc9643e74f
Zooey <zooey@lbk.org.uk>
2017-03-26 12:58:58 +0100

    add timer functionality I've added the main functionality of the timer, it shoots out notifications at the expected intervals, currently the only way to stop the timer is to use ctrl+c to get out of it running in terminal, I 'd like to implement a better command, and to make it give you back the prompt as standard.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 #! /usr/bin/env node
+const notifier = require('node-notifier');
+const path = require('path');
+
+
+// 1 Hour = 3,600,000 Milliseconds
+// 20 mins = 1,200,000 Milliseconds
+const startTime = Date.now();
+const logo = path.join(__dirname, 'images', 'FACicon.png');
+
+notifier.notify({
+  title: 'Timer started!',
+  message: 'Get researching in your pair',
+  icon: logo,
+  sound: true,
+  closeLabel: 'Close',
+});
+
+const timer = setInterval(checkTime, 5000); // eslint-disable-line no-use-before-define
+
+let timer1 = false;
+let timer2 = false;
+
+function checkTime() {
+  const newTime = Date.now();
+
+  if (newTime >= startTime + 3600000 /* 180000*/) {
+    notifier.notify({
+      title: '20/20/20 over',
+      message: 'The full hour has passed, I hope you solved your problem!',
+      icon: logo,
+      sound: true,
+      closeLabel: 'Close',
+    });
+    clearInterval(timer);
+  } else if (newTime >= startTime + 240000 /* 120000*/ && !timer2) {
+    notifier.notify({
+      title: 'Go ask a mentor!',
+      message: 'Another 20 minutes has passed, time to ask a mentor.',
+      icon: logo,
+      sound: true,
+      closeLabel: 'Close',
+    });
+    timer2 = true;
+  } else if (newTime >= startTime + 1200000 /* 60000*/ && !timer1) {
+    notifier.notify({
+      title: '20 minutes is up!',
+      message: 'Ask someone in your cohort for help.',
+      icon: logo,
+      sound: true,
+      closeLabel: 'Close',
+    });
+    timer1 = true;
+  }
+}
